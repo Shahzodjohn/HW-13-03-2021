8b7c0abc6b4e11f3176e36e7266d56c46a9f2a9f
Shahzod Akhmedov <77686662+Shahzodjohn@users.noreply.github.com>
2021-03-14 18:03:34 +0500

    Add files via upload

---
+++ ReverseCS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp59
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 4, 8, 5, 9, 7 };
+            IEnumerable<int> res = numbers.Reverse();
+
+            foreach (var item in res)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
