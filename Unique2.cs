23c359e1a38e466c713b5591a6b711ae44815e38
Shahzod Akhmedov <77686662+Shahzodjohn@users.noreply.github.com>
2021-03-14 17:35:13 +0500

    Add files via upload

---
+++ Unique2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] uniqeNum = { 0, 0, 0.55, 0, 0 };
+            var a = uniqeNum.GroupBy(n => n).Select(g => g.Count() == 1 ? g.Key : 0).Max();
+            Console.WriteLine(a);
+
+        }
+    }
+}
